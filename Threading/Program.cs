fd180bf0146f7d0e53d25962ea1b2d24b72425ae
tnembhard <tnembhard@spartaglobal.com>
2019-02-07 09:45:28 +0000

    First attempt at understanding threading in a C# method.

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threading
 {
@@ -10,6 +11,31 @@ namespace Threading
     {
         static void Main(string[] args)
         {
+            Thread thread1 = new Thread(new ThreadStart(test1));
+            Thread thread2 = new Thread(new ThreadStart(test2));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.Read();
         }
+        static void test1()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine("First Thread is: " + i);
+            }
+            Console.WriteLine("First Thread: Finished");
+        }
+        static void test2()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine("Second Thread is: " + i);
+            }
+            Console.WriteLine("Second Thread: Finished");
+        }
+
     }
+
 }
